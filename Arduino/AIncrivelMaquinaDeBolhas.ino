505278f22de57672a5823386c48c231348ed900c
Victor Pacheco <granix3@gmail.com>
2018-06-06 16:50:31 -0300

    Correção do código

--- Arduino/AIncrivelMaquinaDeBolhas.ino
+++ Arduino/AIncrivelMaquinaDeBolhas.ino
@@ -4,7 +4,7 @@
 @autor: Rafael Mascarenhas Dal Moro
 @coAutores: Gabriel Rodrigues Pacheco
 @resumo: 
-	Projeto de controle de 2 MicroServo Motores que desloca uma haste for coletar solucao saponificada && posiciona-la em frente a um ventilador	e produzir bolhas de sabao. A interface com o usuario se da por um botao de ativacao.	
+	Projeto de controle de 2 MicroServo Motores que desloca uma haste for coletar solucao saponificada && posiciona-la em frente a um VENTILADOR	e produzir bolhas de sabao. A interface com o usuario se da por um BOTAO de ativacao.	
 @ultima alteracao: 01/06/2018
 @licenca: Este projeto está sob a licença do MIT - Acesse "https://github.com/BrinoOficial/AIncrivelMaquinaDeBolhas/blob/master/LICENSE.md" for mais detalhes
 */
@@ -14,27 +14,27 @@
 #define ALTO 94 // angulo de maior altura do braco
 #define BAIXO 43 // angluo de menor altura do braco
 #define MEDIO 65 // angulo intermediario de altura do braco (setCursor de inicio do braco)
-#define SABAO 90 // setCursor da base alinhada com a bacia de sabão
-#define VENTILADOR 15 // setCursor da base alinhada com o ventilador
+#define SABAO 90 // setCursor da base alinhada com o recipiente de sabão
+#define POSICAOVENTILADOR 15 // setCursor da base alinhada com o VENTILADOR
 #define INICIO 45 // setCursor de inicio da base
+#define BOTAO  13 // porta do BOTAO
+#define VENTILADOR  5 // porta do VENTILADOR
 
-int const botao = 6; // porta do botao
-int const ventilador = 7; // porta do ventilador
-int estadoBotao; // guarda leitura do botao
+int estadoBotao; // guarda leitura do BOTAO
 
 Servo base; // motor da base
 Servo braco; // motor do braco
 
 void setup(){
-  pinMode(botao,INPUT_PULLUP);
-  pinMode(ventilador,OUTPUT);
-  braco.attach(5);
-  base.attach(3);
+  pinMode(BOTAO,INPUT_PULLUP);
+  pinMode(VENTILADOR,OUTPUT);
+  braco.attach(6);
+  base.attach(4);
   setCursorInicial();
 }
 
 void loop(){
-  estadoBotao = digitalRead(botao);
+  estadoBotao = digitalRead(BOTAO);
   if(estadoBotao == LOW){
     irParaSabao();
     pegarSabao();
@@ -44,7 +44,7 @@ void loop(){
   setCursorInicial();
 }
 
-void setCursorInicial(){ // posiciona braco && base na setCursor inicial de execucao
+void setCursorInicial(){ // posiciona braco e base na setCursor inicial de execucao
   base.write(INICIO);
   braco.write(MEDIO);
 }
@@ -58,7 +58,7 @@ void irParaSabao(){ // desloca a base for o pote de sabao, levantando a haste
       delay(15);
    }
 }
-void pegarSabao(){ // movimenta o braco for mergulhar a haste no sabao
+void pegarSabao(){ // movimenta o braco para mergulhar a haste no sabao
     for(int i=ALTO; i>=BAIXO; i--){ 
       braco.write(i);
       delay(15);
@@ -68,32 +68,32 @@ void pegarSabao(){ // movimenta o braco for mergulhar a haste no sabao
       braco.write(i);
       delay(15);
     }
-    for(int i=0; i<3;i++){ // "sacode" for remove excesso de liquido
+    for(int i=0; i<3;i++){ // "sacode" para remover excesso de liquido
       braco.write(89);
       delay(120);
       braco.write(94);
       delay(120);
    }
 }
-void irParaVentilador(){ // desloca a base for o ventilador
- for(int i= SABAO;i >= VENTILADOR; i--){ 
+void irParaVentilador(){ // desloca a base para o VENTILADOR
+ for(int i= SABAO;i >= POSICAOVENTILADOR; i--){ 
       base.write(i);
       delay(15);
     }
 }
-void ventilar(){ // Aciona && desliga o ventilador
-digitalWrite(ventilador,HIGH);
+void ventilar(){ // Aciona e desliga o VENTILADOR
+digitalWrite(VENTILADOR,HIGH);
 	delay(1500);
-	digitalWrite(ventilador,LOW);
+	digitalWrite(VENTILADOR,LOW);
 	delay(500);
 }
 
-// funcao abaixo retornaria gradualmente a setCursor inicial, porem o operador "for" apresenta erros na compilacao
-/*void voltarInicio(){ 	
-	for(int i= ALTO, int j = VENTILADOR; i>= MEDIO, j<= INICIO; i++,j++){
+// funcao abaixo retorna gradualmente ao setCursor inicial, porem o operador "para" apresenta erros na compilacao
+void voltarInicio(){ 	
+	for(int i= ALTO,  j = POSICAOVENTILADOR; i>= MEDIO, j<= INICIO; i++,j++){
 		base.write(j);
 		braco.write(i);
 		delay(15);
 	}
 }
-*/
+
